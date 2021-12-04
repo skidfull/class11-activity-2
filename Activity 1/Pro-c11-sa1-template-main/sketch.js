d2422895a0390274a9a6317e7f90fab3edfec5e7
skidfull <95112430+skidfull@users.noreply.github.com>
2021-12-04 22:23:11 +0800

    Add files via upload

---
+++ Activity 1/Pro-c11-sa1-template-main/sketch.js
@@ -0,0 +1,28 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+
+function Weight_average() {
+  var sum = 0
+  for( var i = 0; i<weight.length; i = i+1) {
+    sum = sum + weight[i]
+  }
+var average_weight = sum/weight.length
+console.log(average_weight)
+
+
+}
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+Weight_average()
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
